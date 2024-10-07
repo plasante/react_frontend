44fe2c2dad747e6f3f2a472901a124b7ba833e88
Pierre Lasante <pierre.lasante@videotron.ca>
2024-10-07 11:37:14 -0400

    Check if isLoggedIn in the SamplePage.js

--- src/pages/extra-pages/SamplePage.js
+++ src/pages/extra-pages/SamplePage.js
@@ -1,18 +1,30 @@
 // material-ui
 import { Typography } from '@mui/material';
-
-// project import
 import MainCard from 'components/MainCard';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
-const SamplePage = () => (
-  <MainCard title="Sample Card">
-    <Typography variant="body2">
-      Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
-      minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in reprehended
-      in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui officiate
-      descent molls anim id est labours.
-    </Typography>
-  </MainCard>
-);
+const SamplePage = () => {
+  const navigate = useNavigate();
+
+  // This is executed when the application mount
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem('token');
+    if (!isLoggedIn) {
+      navigate('/login');
+    }
+  });
+
+  return (
+    <MainCard title="Sample Card">
+      <Typography variant="body2">
+        Lorem ipsum dolor sit amen, consenter nipissing eli, sed do elusion tempos incident ut laborers et doolie magna alissa. Ut enif ad
+        minim venice, quin nostrum exercitation illampu laborings nisi ut liquid ex ea commons construal. Duos aube grue dolor in
+        reprehended in voltage veil esse colum doolie eu fujian bulla parian. Exceptive sin ocean cuspidate non president, sunk in culpa qui
+        officiate descent molls anim id est labours.
+      </Typography>
+    </MainCard>
+  );
+};
 
 export default SamplePage;
